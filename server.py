481e33d76b8a374a0901dcbde130c14676573023
b-esc <ben.escobar.ben@gmail.com>
2023-12-19 19:19:40 -0600

    Template for https server and file upload server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 # https://flask.palletsprojects.com/en/3.0.x/quickstart/#variable-rules
 
@@ -11,7 +11,7 @@ storage = {}
 # Key-Value server
 @app.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
 @app.route('/set_key', methods=['POST'])
 def set_key():
@@ -20,33 +20,76 @@ def set_key():
     k = json.get('key')
     storage[k] = v
     # https://flask.palletsprojects.com/en/3.0.x/quickstart/#about-responses
-    message = jsonify({'message': f'Key:{k} has been set to {v}'})
+    response = jsonify({'message': f'Key:{k} has been set to {v}'})
+    return response
 
 @app.route('/request_key', methods=['POST'])
 def request_key():
-    pass
+    k = request.json.get('key')
+    v = storage.get(k, f'Value not found for {k}')
+    response = jsonify({'key': k, 'value': v})
+    return response
 
 @app.route('/request_all_current_contents', methods=['POST'])
 def request_all_current_contents():
-    pass
+    response = jsonify(storage)
+    return response
+
 
 @app.route('/delete_key', methods=['POST'])
 def delete_key():
-    pass
+    k = request.json.get('key')
+    if not k in storage:
+        response = jsonify({'error': f'Key: {k} is not in storage'})
+        return response, 404
+    else:
+        del storage[k]
+        response = jsonify({'message': f'Key: {k} has been deleted'})
+        return response
 
 @app.route('/clear_all_current_contents', methods=['POST'])
 def clear_all_current_contents():
-    pass
+    # https://www.programiz.com/python-programming/methods/dictionary/clear
+    storage.clear()
+    response = jsonify({'message': 'All contents have been cleared from storage'})
+    return response
 
 # File Upload Server
+# https://flask.palletsprojects.com/en/3.0.x/patterns/fileuploads/
+
+UPLOADED_FILES = 'uploaded_files'
+app.config['UPLOAD_FOLDER'] = UPLOADED_FILES
+UPLOAD_FOLDER = app.config['UPLOAD_FOLDER']
+
 @app.route('/upload_file', methods=['POST'])
 def upload_file():
-    pass
+    no_file_response = jsonify({'error': 'There is not a file that has been selected for upload'})
+    if 'file' not in request.files:
+        return no_file_response, 400
+
+    file_upload = request.files['file']
+
+    if file.filename == '':
+        return no_file_response, 400
+
+    file_destination = os.path.join(UPLOAD_FOLDER, file_upload.filename)
+    file_upload.save(file_destination)
+    response = jsonify({'message': f'File {file_upload.filename} has been uploaded'})
+    return response
+
+
 
 @app.route('/request_uploaded_files', methods=['GET'])
 def request_uploaded_files():
-    pass
+    filenames = os.listdir(UPLOAD_FOLDER)
+    response = jsonify({'uploaded_files': filenames})
+    return response
 
-@app.route('request_file_for_download', methods=['GET'])
-def request_file_for_download():
-    pass
+@app.route('request_file_for_download/<file>', methods=['GET'])
+def request_file_for_download(file):
+    downloaded_file = send_from_directory(UPLOAD_FOLDER, file)
+    return downloaded_file
+
+# https://pythonhow.com/python-tutorial/flask/How-a-Flask-app-works/
+if __name__ == '__main__':
+    app.run(debug = True)
