ac5b59a7d0e1306a0279299f51429f661628b504
b-esc <ben.escobar.ben@gmail.com>
2023-12-19 19:57:27 -0600

    Added index.js and initial function definitions for form interaction

---
+++ templates/index.js
@@ -0,0 +1,32 @@
+// https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+async function setKey() {
+
+}
+
+async function requestKey() {
+
+}
+
+async function requestAllCurrentContents() {
+
+}
+
+async function deleteKey() {
+
+}
+
+async function clearAllCurrentContents() {
+
+}
+
+async function uploadFile() {
+
+}
+
+async function requestUploadedFiles() {
+
+}
+
+async function requestFileForDownload() {
+
+}
